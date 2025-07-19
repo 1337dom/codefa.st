329a9db23481cd159fec2bb7a0bb1977f4cc9852
1337dom <dominikstruben@outlook.de>
2025-07-19 23:05:10 +0200

    initial commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Collect customer feedback to build a better product</h1>
+      <div>
+        Create a feedback board in minutes, prioritize features, and build
+        products your customers will love.
+      </div>
+    </main>
+  );
+}
